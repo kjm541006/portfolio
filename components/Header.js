4423341074ee70dda646a7d6ff347ca519ca8f50
jumin <kjm541113@gmail.com>
2024-02-15 21:17:44 +0900

    반응형 nav 햄버거버튼 구현

--- components/Header.js
+++ components/Header.js
@@ -4,6 +4,11 @@ import { useEffect, useState } from "react";
 
 const Header = () => {
   const [isNavbarWhite, setIsNavbarWhite] = useState(false);
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuActive(!isMenuActive);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -27,8 +32,40 @@ const Header = () => {
       <Image className={styles.header_img} src={"/images/img.jpg"} layout="fill" objectFit="cover" objectPosition="center" />
       <nav className={`${styles.navbar} ${isNavbarWhite ? styles.navbar_white : ""}`}>
         <div className={styles.nav_content}>
-          <div className={styles.name}>주민의 포트폴리오</div>
+          <div className={styles.name}>주민의 포트폴리오(개발중)</div>
           <div className={styles.menu}>
+            <div>
+              <a href="#aboutme">About me</a>
+            </div>
+            <div>
+              <a href="#contact">Contact</a>
+            </div>
+            <div>
+              <a href="#skills">Skills</a>
+            </div>
+            <div>
+              <a href="#projects">Projects</a>
+            </div>
+            <div>
+              <a href="#"></a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      {/* <div className={`${styles.menu} ${isMenuActive ? styles.menu_active : ""}`}>
+        <nav className={styles.hamburger_menu} onClick={toggleMenu}>
+          <div className={styles.nav_content}>
+            <div className={styles.name}>주민의 포트폴리오</div>
+            <div className={styles.hamburger}>
+              <Image
+                className={styles.hamburger_img}
+                src={"/images/hamburger.png"}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+              />
+              안녕
+            </div>
             <div>
               <a href="#aboutme">About me</a>
             </div>
@@ -45,8 +82,8 @@ const Header = () => {
               <a href="#">띠용</a>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div> */}
       <div className={styles.header_main}>
         <div className={styles.header_main_content}>
           <div className={styles.header_main_title}>
