9a675134892e02ac13a59e630f9e24b606dd7248
kjm541006 <kjm541113@gmail.com>
2024-02-20 22:59:47 +0900

    header 버그 수정

--- components/Header.js
+++ components/Header.js
@@ -3,27 +3,70 @@ import styles from "./Header.module.css";
 import { useEffect, useState } from "react";
 
 const Header = () => {
-  const [isNavbarWhite, setIsNavbarWhite] = useState(false);
-  const [isMenuActive, setIsMenuActive] = useState(false);
+  const [isNavbarWhite, setIsNavbarWhite] = useState(null);
+  // const [isMenuActive, setIsMenuActive] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(null);
 
   const toggleMenu = () => {
     setIsMenuActive(!isMenuActive);
   };
 
+  // useEffect(() => {
+  //   const checkScreenSize = () => {
+  //     setIsSmallScreen(window.innerWidth <= 1023);
+  //   };
+
+  //   const handleScroll = () => {
+  //     const y = window.scrollY;
+  //     if (y >= 100) {
+  //       setIsNavbarWhite(true);
+  //     } else {
+  //       setIsNavbarWhite(false);
+  //     }
+  //   };
+
+  //   window.addEventListener("resize", checkScreenSize);
+  //   window.addEventListener("scroll", handleScroll);
+
+  //   // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거
+  //   return () => {
+  //     window.removeEventListener("resize", checkScreenSize);
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
+
   useEffect(() => {
-    const handleScroll = () => {
-      const y = window.scrollY;
-      if (y >= 100) {
-        setIsNavbarWhite(true);
-      } else {
-        setIsNavbarWhite(false);
-      }
-    };
+    let checkScreenSize;
+    let handleScroll;
 
-    window.addEventListener("scroll", handleScroll);
+    if (typeof window !== "undefined") {
+      checkScreenSize = () => {
+        setIsSmallScreen(window.innerWidth <= 1023);
+      };
 
+      handleScroll = () => {
+        const y = window.scrollY;
+        if (y >= 100) {
+          setIsNavbarWhite(true);
+        } else {
+          setIsNavbarWhite(false);
+        }
+      };
+
+      // 컴포넌트가 마운트될 때 현재 윈도우 너비를 설정합니다.
+      checkScreenSize();
+      handleScroll();
+
+      window.addEventListener("resize", checkScreenSize);
+      window.addEventListener("scroll", handleScroll);
+    }
+
+    // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      if (typeof window !== "undefined") {
+        window.removeEventListener("resize", checkScreenSize);
+        window.removeEventListener("scroll", handleScroll);
+      }
     };
   }, []);
 
@@ -53,13 +96,13 @@ const Header = () => {
 
   return (
     <header className={styles.header}>
-      <Image className={styles.header_img} src={"/images/img.jpg"} layout="fill" objectFit="cover" objectPosition="center" />
+      <Image className={styles.header_img} src={"/images/img.jpg"} width={640} height={360} />
       <nav className={`${styles.navbar} ${isNavbarWhite ? styles.navbar_white : ""}`}>
         <div className={styles.nav_content}>
           <div className={styles.name} onClick={goToTop}>
             주민의 포트폴리오(개발중)
           </div>
-          <div className={styles.menu}>
+          <div className={`${isSmallScreen ? styles["hamburger_menu"] : styles.menu}`}>
             <div onClick={() => goToAboutme()}>About me</div>
             <div onClick={() => goToContact()}>Contact</div>
             <div onClick={() => goToSkills()}>Skills</div>
