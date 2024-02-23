0f91ce648752920df989d92e595fcaec3de5e778
kjm541006 <kjm541113@gmail.com>
2024-02-23 18:36:50 +0900

    헤더 반응형 완료

--- components/Header.js
+++ components/Header.js
@@ -17,14 +17,13 @@ const Header = () => {
 
       handleScroll = () => {
         const y = window.scrollY;
-        if (y >= 100) {
+        if (y >= 100 || isSmallScreen) {
           setIsNavbarWhite(true);
         } else {
           setIsNavbarWhite(false);
         }
       };
 
-      // 컴포넌트가 마운트될 때 현재 윈도우 너비를 설정합니다.
       checkScreenSize();
       handleScroll();
 
@@ -32,14 +31,18 @@ const Header = () => {
       window.addEventListener("scroll", handleScroll);
     }
 
-    // 컴포넌트가 언마운트될 때 이벤트 리스너를 제거
     return () => {
       if (typeof window !== "undefined") {
         window.removeEventListener("resize", checkScreenSize);
         window.removeEventListener("scroll", handleScroll);
       }
     };
-  }, []);
+  }, [isSmallScreen, isNavbarWhite]);
+
+  const toggleHamburgerMenuVisible = () => {
+    let hamburgerMenu = document?.querySelector(`.${styles.nav_content_mobile}`);
+    hamburgerMenu?.classList.toggle(`${styles.nav_content_mobile_visible}`);
+  };
 
   const goToTop = () => {
     window.scrollTo(0, 0);
@@ -48,21 +51,25 @@ const Header = () => {
   const goToAboutme = () => {
     let topAboutMe = document.getElementById("aboutme");
     topAboutMe.scrollIntoView();
+    toggleHamburgerMenuVisible();
   };
 
   const goToContact = () => {
     let topContact = document.getElementById("contact");
     topContact.scrollIntoView();
+    toggleHamburgerMenuVisible();
   };
 
   const goToSkills = () => {
     let topSkills = document.getElementById("skills");
     topSkills.scrollIntoView();
+    toggleHamburgerMenuVisible();
   };
 
   const goToProjects = () => {
     let topProjects = document.getElementById("projects");
     topProjects.scrollIntoView();
+    toggleHamburgerMenuVisible();
   };
 
   return (
@@ -71,18 +78,29 @@ const Header = () => {
       <nav className={`${styles.navbar} ${isNavbarWhite ? styles.navbar_white : ""}`}>
         <div className={styles.nav_content}>
           <div className={styles.name} onClick={goToTop}>
-            주민의 포트폴리오(개발중)
+            주민의 포트폴리오
           </div>
-          <div className={`${isSmallScreen ? styles["hamburger_menu"] : styles.menu}`}>
+          {!isSmallScreen ? (
+            <div className={styles.menu}>
+              <div onClick={() => goToAboutme()}>About me</div>
+              <div onClick={() => goToContact()}>Contact</div>
+              <div onClick={() => goToSkills()}>Skills</div>
+              <div onClick={() => goToProjects()}>Projects</div>
+            </div>
+          ) : (
+            <div className={styles.hamburger} onClick={() => toggleHamburgerMenuVisible()}>
+              <Image src={"/images/hamburger.svg"} width={40} height={40} />
+            </div>
+          )}
+        </div>
+        {isSmallScreen ? (
+          <div className={styles.nav_content_mobile}>
             <div onClick={() => goToAboutme()}>About me</div>
             <div onClick={() => goToContact()}>Contact</div>
             <div onClick={() => goToSkills()}>Skills</div>
             <div onClick={() => goToProjects()}>Projects</div>
-            <div>
-              <a href="#"></a>
-            </div>
           </div>
-        </div>
+        ) : null}
       </nav>
       <div className={styles.header_main}>
         <div className={styles.header_main_content}>
