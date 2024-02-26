294f43f3a2d6cd55fa39ce2872e88a1b18b5a8b0
kjm541006 <kjm541113@gmail.com>
2024-02-26 16:07:14 +0900

    about me 최적화 버그 수정

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -54,13 +54,12 @@
 // };
 
 // export default AboutMe;
-
 import styles from "./AboutMe.module.css";
 
-const InfoSection = ({ title, content, link, linkClass }) => (
-  <div className={styles.info_section}>
-    <div className={styles.info_section_title}>{title}</div>
-    <div className={styles.info_section_content}>
+const InfoSection = ({ sectionClass, title, content, link, linkClass, titleClass, contentClass }) => (
+  <div className={sectionClass}>
+    <div className={titleClass}>{title}</div>
+    <div className={contentClass}>
       {link ? (
         <a href={link} className={linkClass}>
           {content}
@@ -80,10 +79,34 @@ const AboutMe = () => {
           <div className={styles.aboutme_title_text}>About me</div>
         </div>
         <div className={styles.aboutme_info}>
-          <InfoSection title="이름" content="김주민" />
-          <InfoSection title="생년월일" content="1997.05.04" />
-          <InfoSection title="거주" content="서울시 광진구" />
-          <InfoSection title="학력" content="가톨릭대학교 컴퓨터정보공학부" />
+          <InfoSection
+            sectionClass={styles.aboutme_info_name}
+            title="이름"
+            content="김주민"
+            titleClass={styles.aboutme_info_name_title}
+            contentClass={styles.aboutme_info_name_content}
+          />
+          <InfoSection
+            sectionClass={styles.aboutme_info_birth}
+            title="생년월일"
+            content="1997.05.04"
+            titleClass={styles.aboutme_info_birth_title}
+            contentClass={styles.aboutme_info_birth_content}
+          />
+          <InfoSection
+            sectionClass={styles.aboutme_info_loc}
+            title="거주"
+            content="서울시 광진구"
+            titleClass={styles.aboutme_info_loc_title}
+            contentClass={styles.aboutme_info_loc_content}
+          />
+          <InfoSection
+            sectionClass={styles.aboutme_info_edu}
+            title="학력"
+            content="가톨릭대학교 컴퓨터정보공학부"
+            titleClass={styles.aboutme_info_edu_title}
+            contentClass={styles.aboutme_info_edu_content}
+          />
         </div>
       </div>
       <div className={styles.contact_wrapper} id="contact">
@@ -91,8 +114,24 @@ const AboutMe = () => {
           <div className={styles.contact_title_text}>Contact</div>
         </div>
         <div className={styles.contact_info}>
-          <InfoSection title="연락처" content="010-2831-7006" link="tel:010-2831-7006" linkClass={styles.contact_phone} />
-          <InfoSection title="이메일" content="kjm541113@gmail.com" link="mailto:kjm541113@gmail.com" linkClass={styles.contact_email} />
+          <InfoSection
+            sectionClass={styles.contact_info_phone}
+            title="연락처"
+            content="010-2831-7006"
+            link="tel:010-2831-7006"
+            linkClass={styles.contact_phone}
+            titleClass={styles.contact_info_phone_title}
+            contentClass={styles.contact_info_phone_content}
+          />
+          <InfoSection
+            sectionClass={styles.contact_info_email}
+            title="이메일"
+            content="kjm541113@gmail.com"
+            link="mailto:kjm541113@gmail.com"
+            linkClass={styles.contact_email}
+            titleClass={styles.contact_info_email_title}
+            contentClass={styles.contact_info_email_content}
+          />
         </div>
       </div>
     </>
